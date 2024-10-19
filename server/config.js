989ef8b73b83527433c8c4c03531ac076b3fd698
Saad Shaik <saad.shaik321@gmail.com>
2024-10-19 05:33:13 +0530

    remove keys

--- server/config.js
+++ server/config.js
@@ -1,6 +1,5 @@
 module.exports = {
-    MONGO_URI: 'mongodb+srv://admin:admin42201@cluster0.rbpsh.mongodb.net/posterDB?retryWrites=true&w=majority',  // or MongoDB Atlas URI
-    GOOGLE_API_KEY: 'AIzaSyCxCg81ODfkFIJtgaN9cZJXgivctb1_qfI',  // Replace with actual Gemini OCR API
-    // GEMINI_TRANSLATION_API: 'https://gemini-translation-api-endpoint'  // Replace with actual Gemini Translation API
+    MONGO_URI: '<Your URI>',  // or MongoDB Atlas URI
+    GOOGLE_API_KEY: '<Your Key>',  // Replace with actual Gemini OCR API
   };
   
